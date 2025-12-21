ca6b12678847e0aa2dd2992015db6453039a424b
Yytsi <tuukka.yildirim@gmail.com>
2025-12-21 23:12:41 +0200

    day4 dict comprehension¨

--- 2025/day4/sol2.py
+++ 2025/day4/sol2.py
@@ -1,8 +1,7 @@
 M,c,h=140,0,{}
 r,g=range(n:=M-2),h
 
-for y in r:s = input();[h.__setitem__(y*M+x,46<ord(s[x]))for x in r]
-    
+for y in r:s = input();h|={y*M+x:46<ord(s[x])for x in r}
 for k in r:
     for k in h: (h[k]and sum(h.get((k//M+(4<j)-(j<3))*M+k%M+~((3^j)%~2),0)for j in r[:8])<4)and(g.__setitem__(k,0),c:=c+1)
     h = g

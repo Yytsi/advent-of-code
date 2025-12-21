7ee701af2e9cbe27da5fa9b5c9601befc35a2cba
Yytsi <tuukka.yildirim@gmail.com>
2025-12-21 23:10:13 +0200

    day4 improved

--- 2025/day4/sol2.py
+++ 2025/day4/sol2.py
@@ -1,14 +1,10 @@
 M,c,h=140,0,{}
 r,g=range(n:=M-2),h
 
-for y in r:
-    s = input()
-    for x in r:h[y*M+x]=46<ord(s[x])
+for y in r:s = input();[h.__setitem__(y*M+x,46<ord(s[x]))for x in r]
     
 for k in r:
-    for y in r:
-        for x in r:
-            if h[y*M+x] and sum(h.get((y+(4<j)-(j<3))*M+x+~((3^j)%~2),0)for j in r[:8]) < 4: g[y*M+x] = 0; c += 1
+    for k in h: (h[k]and sum(h.get((k//M+(4<j)-(j<3))*M+k%M+~((3^j)%~2),0)for j in r[:8])<4)and(g.__setitem__(k,0),c:=c+1)
     h = g
 
 print(c)

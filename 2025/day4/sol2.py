b4bdea729e35af85b4a8155867f5abdbe8834105
Yytsi <tuukka.yildirim@gmail.com>
2025-12-21 21:34:52 +0200

    remove 2 lines

--- 2025/day4/sol2.py
+++ 2025/day4/sol2.py
@@ -8,9 +8,7 @@ for y in r:
 for k in r:
     for y in r:
         for x in r:
-            if h[y*M+x]<1:continue
-            t=sum(h.get((y+(4<j)-(j<3))*M+x+~((3^j)%~2),0)for j in r[:8])
-            if t < 4: g[y*M+x] = 0; c += 1
+            if h[y*M+x] and sum(h.get((y+(4<j)-(j<3))*M+x+~((3^j)%~2),0)for j in r[:8]) < 4: g[y*M+x] = 0; c += 1
     h = g
 
 print(c)

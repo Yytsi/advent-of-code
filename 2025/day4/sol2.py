1c37e56059e58ed4a5787746d77a46fa26bc5c71
Yytsi <tuukka.yildirim@gmail.com>
2025-12-21 23:17:19 +0200

    day4 improved

--- 2025/day4/sol2.py
+++ 2025/day4/sol2.py
@@ -3,7 +3,7 @@ r,g=range(n:=M-2),h
 
 for y in r:s = input();h|={y*M+x:46<ord(s[x])for x in r}
 for k in r:
-    for k in h: (h[k]and sum(h.get((k//M+(4<j)-(j<3))*M+k%M+~((3^j)%~2),0)for j in r[:8])<4)and(g.__setitem__(k,0),c:=c+1)
+    for k in h: h[k]and sum(h.get((k//M+(4<j)-(j<3))*M+k%M+~((3^j)%~2),0)for j in r[:8])<4 and(g:=g|{k:0},c:=c+1)
     h = g
 
 print(c)
